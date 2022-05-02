a80e75d2cbf7cec52454f02c751c2e066c2cb7a4
travistrotto <travis.trotto@gmail.com>
2022-05-02 16:01:59 -0400

    working prototype

--- generate.js
+++ generate.js
@@ -1,28 +1,31 @@
 function gen() {
-    r1 = Math.floor(Math.random() * 4);
-    r2 = Math.floor(Math.random() * 4);
-    r3 = Math.floor(Math.random() * 4);
-    r4 = Math.floor(Math.random() * 4);
-
-    job = "";
-    
-    if (r1 == 0) {
-        job = "Warrior"
-      } else if (r1 == 2) {
-        job = "Monk"
-      } else if (r1 == 3) {
-        job = "White Mage"
-      } else {
-        job = "Black Mage"
-      }
-
-    document.getElementById("demo").innerHTML = job;
+    party1 = genJob();
+    party2 = genJob();
+    party3 = genJob();
+    party4 = genJob();
+    document.getElementById("party1").innerHTML = party1;
+    document.getElementById("party2").innerHTML = party2;
+    document.getElementById("party3").innerHTML = party3;
+    document.getElementById("party4").innerHTML = party4;
     // expected output: 0, 1, 2 or 3.
     
-    // var values = ["test","values","go","here"],
-    var values = [r1,r1],
-        valueToUse = values[Math.floor(Math.random() * values.length)];
-    // do something with the selected value
-    // alert(valueToUse);
-    return(valueToUse);
+    return(0);
+}
+
+function genJob() {
+  randomValue = Math.floor(Math.random() * 6);
+  if (randomValue == 0) {
+    job = "Warrior"
+  } else if (randomValue == 1) {
+    job = "Thief"
+  } else if (randomValue == 2) {
+    job = "Monk"
+  } else if (randomValue == 3) {
+    job = "White Mage"
+  } else if (randomValue == 4) {
+    job = "Red Mage"
+  } else {
+    job = "Black Mage"
+  }
+  return job;
 }
